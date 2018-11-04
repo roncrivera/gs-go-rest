f95c583778a0801cdb53b5dce16c65493673202a
Ron Rivera <roncrivera@gmail.com>
2018-11-04 20:52:15 +1100

    Initial commit.

---
+++ models/movie.go
@@ -0,0 +1,6 @@
+type Movie struct {
+    ID          bson.ObjectId `bson:"_id" json:"id"`
+    Name        string        `bson:"name" json:"name"`
+    CoverImage  string        `bson:"cover_image" json:"cover_image"`
+    Description string        `bson:"description" json:"description"`
+}
